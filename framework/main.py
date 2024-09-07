46a21b84642a2bb53daafde236f67029c17eafc7
RZJournal <rzjournal@rzjournal.com>
2024-09-07 18:21:55 -0400

    Add main.py

---
+++ framework/main.py
@@ -0,0 +1,12 @@
+from algorithm import algorithm
+from models import createDefaultModelList
+
+def main():
+    T = int(input("Input total amount of requests: "))  
+    c = float(input("Input hyperparameter c: "))  
+    m_list = createDefaultModelList()  
+
+    algorithm(T, c, m_list)  
+
+if __name__ == "__main__":
+    main()
