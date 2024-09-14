671efe0fc2d9dbfd0bae006c1613dd65168f5294
ryanzhangofficial <ryzhangofficial@gmail.com>
2024-09-14 20:14:38 +0200

    Update accuracy_metrics

--- framework/utilities/metrics.py
+++ framework/utilities/metrics.py
@@ -7,19 +7,19 @@ def calculate_bleu(generated_output, reference_output):
     reference_tokens = [nltk.word_tokenize(reference_output)]
     smoothie = SmoothingFunction().method4
     bleu_score = sentence_bleu(reference_tokens, generated_tokens, smoothing_function=smoothie)
-    return bleu_score * 100
+    return bleu_score
 
 def calculate_rouge(generated_output, reference_output):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
     scores = scorer.score(reference_output, generated_output)
     return {
-        'rouge1': scores['rouge1'].fmeasure * 100,
-        'rouge2': scores['rouge2'].fmeasure * 100,
-        'rougeL': scores['rougeL'].fmeasure * 100
+        'rouge1': scores['rouge1'].fmeasure,
+        'rouge2': scores['rouge2'].fmeasure,
+        'rougeL': scores['rougeL'].fmeasure
     }
 
 def calculate_exact_match(generated_output, reference_output):
     if generated_output.strip() == reference_output.strip():
-        return 100.0
+        return 1
     else:
-        return 0.0
+        return 0
