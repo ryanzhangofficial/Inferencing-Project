0eb10c9420151a0d9de3067ecf92e4aa49f2caf4
ryanzhangofficial <ryzhangofficial@gmail.com>
2024-09-20 06:44:16 +0200

    Update power_monitoring experiments

---
+++ power_monitoring/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,17 @@
+"""
+This script shows how to initialize, start, and stop the hardware monitor. 
+"""
+import threading
+
+from .monitor import HWMonitor
+
+
+class Client(object): 
+  
+  def __init__(self): 
+    # This automatically starts the hardware monitor when a job is launched.
+    self.monitor = HWMonitor(monitoring_freq=1, stop_event=threading.Event())
+    
+  def stop_monitor(self): 
+    # This stops the asynchronous hardware monitor. 
+    self.monitor.stop_monitor.set()
